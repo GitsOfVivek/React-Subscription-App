014f62837fe7e72dd0bb6bf3fa0c8ab62dd59850
ViveK <95522287+GitsOfVivek@users.noreply.github.com>
2022-12-20 22:42:36 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -4,18 +4,26 @@ const App = () => {
   return (
     <div id="main">
       <div class="container">
-                <div class="community">
-                  <h2 class="community-header">Join our community</h2>
-                </div>
-                <div class="card-details">
-                  <div class="card">
-                    <p class="card-header">Monthly Subscription</p>
-                  </div>
-                  <div class="why-us">
-                    <h4 class="why-us-header">Why Us</h4>
-                  </div>
-                </div>
-              </div>
+        <div class="community">
+          <h2 class="community-header">Join our community</h2>
+          <h3>30-day, hassle-free monkey back gurarantee</h3>
+          <p>aaaaaaaaaaaaaaaaaaaaa</p>
+        </div>
+        <div class="card-details">
+          <div class="card">
+            <p class="card-header">Monthly Subscription</p>
+          </div>
+          <div class="why-us">
+            <h4 class="why-us-header">Why Us</h4>
+            <p>Tutorials by industry experts</p>
+            <p>Peer & expert code review</p>
+            <p>Coding exercises</p>
+            <p>Access yo our Github repos</p>
+            <p>Community forum</p>
+            <p>Flashcard decks</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
