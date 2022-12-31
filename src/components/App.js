5981a07198cff43e5277b5db2c80160015c6bea3
ViveK <95522287+GitsOfVivek@users.noreply.github.com>
2022-12-31 13:24:33 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -3,48 +3,49 @@ import "../styles/App.css";
 const App = () => {
   return (
     <div id="main">
-			<div class="container">
-				<div class="community">
-					<h2 class="community-header">Join our community</h2>
-					<ul class="community-more">
-						<li class="sub">
-							30-day, hassle free money back guarantee
-						</li>
-						<li class="more-info">
-							Gain access to our full library of tutorials along
-							with expert code reviews.
-						</li>
-						<li class="more-info">
-							Perfect for any developers
-							who are serious about honing their skills.
-						</li>
-					</ul>
-				</div>
-				<div class="card-details">
-					<div class="card">
-						<p class="card-header">Monthly Subscription</p>
-						<div class="price">
-							<div>
-								<span class="big-text">$29</span
-								><span class="px">Per Month</span>
-							</div>
-							<div>Full access for less than $1 a day.</div>
-						</div>
-						<button class="btn">Sign Up</button>
-					</div>
-					<div class="why-us">
-						<h4 class="why-us-header">Why Us</h4>
-						<ul class="feature">
-							<li>Tutorials by industry experts</li>
-							<li>Peer & expert code review</li>
-							<li>Coding exercises</li>
-							<li>Access yo our Github repos</li>
-							<li>Community forum</li>
-							<li>Flashcard decks</li>
-						</ul>
-					</div>
-				</div>
-			</div>
+			<section class="card-section">
+              <div class="container">
+                <div class="community">
+                  <h2 class="community-header">Join our community</h2>
+                  <h3 class="community-sub-header">
+                    30-day, hassle-free monkey back gurarantee
+                  </h3>
+                  <p class="community-text">
+                    Gain access to our full library of tutorials along with expert
+                    code reviews. Perfect for any developers who are serious about
+                    honing their skills.
+                  </p>
+                </div>
+                <div class="card-details">
+                  <div class="card">
+                    <p class="card-header">Monthly Subscription</p>
+                    <div class="price">
+                      <span>$29</span>
+                      <p>Per Month</p>
+                    </div>
+                    <p class="card-details-text">
+                      Full access for less than $1 a day.
+                    </p>
+                    <a href="#" class="btn btn-card">
+                      Sign Up
+                    </a>
+                  </div>
+                  <div class="why-us">
+                    <h4 class="why-us-header">Why Us</h4>
+                    <div class="why-us-lists">
+                      <ul class="why-us-items">
+                        <li class="why-us-item">Tutorials by industry experts</li>
+                        <li class="why-us-item">Peer & expert code review</li>
+                        <li class="why-us-item">Coding exercises</li>
+                        <li class="why-us-item">Access yo our GitHub repos</li>
+                        <li class="why-us-item">Community forum</li>
+                        <li class="why-us-item">Flashcard decks</li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
 		</div>
   );
 };
